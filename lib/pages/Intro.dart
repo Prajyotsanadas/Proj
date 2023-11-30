11a4afb0e5ddcd78ee5d0c7cb7269538f65e737d
Nikisha Joshi <joshinikishanikishajoshi@gmail.com>
2023-11-30 13:00:19 +0545

    ni

--- lib/pages/Intro.dart
+++ lib/pages/Intro.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      debugShowCheckedModeBanner: false,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -42,15 +43,11 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      body: Container(
 
-        title: Text(widget.title),
+        color: Colors.green,
+          
       ),
-      body: Center(
-
-        child: Text(""),
-      )// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
